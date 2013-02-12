7fd9e6cf4da008dcc357345d007bf7df525203ed
mohamed camara <mcamara@cs.umb.edu>
2013-02-12 14:35:20 -0500

    lab2 and memo

--- Lab2/BingoCard.java
+++ Lab2/BingoCard.java
@@ -53,6 +53,75 @@ public class BingoCard
 		}
 		return s;
 	}
+	public boolean cover(BingoSquare mycover)
+	{
+		boolean covered = true;
+
+		for(int i=0; i<card.length; i++)
+		{
+			for(int j=0; j<card[i].length; j++)
+			{
+				if(mycover.equals(card[i][j]))
+				{
+					cover[i][j] = null;
+					return true;
+				}
+			}
+		}
+		return covered;
+	}
+	public boolean hasBingo()
+	{
+		boolean bingo = true;
+
+		//row checking
+
+		for(int i=0; i<card.length; i++)
+		{
+			for(int j=0; j<card[i].length; j++)
+			{
+				if(card[i][j] != null)
+				return false;
+			}
+			if(false)
+				return bingo;
+		}
+		//column checking
+
+		for(i=0; i<card.length; i++)
+		{
+			for(int j=0; j<card[i].length; j++)
+			{
+				if(card[j][i] != null)
+					return false;
+			}
+			if(false)
+				return bingo;
+		}
+		//left diagonal checking
+
+		for(int i=0; i<card.lenght; i++)
+		{
+			if(card[i][i] != null)
+				return false;
+		}
+		if(false)
+			return bingo;
+
+		//right diagonal checking
+
+		for(int i=0; i<card.length; i++)
+		{
+			if(card[i][j] != null)
+					return false;
+			j--;
+			}
+		if(false)
+			return bingo;
+
+		return false;
+		}
+	}
 }
 
 	
